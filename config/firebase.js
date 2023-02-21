31f4efadc5ce0f6373a13b48569065e5a90a8c87
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-02-21 23:54:45 +0530

    Install firebase and create firebase config

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvV1n7iwfnHr5HrdfadD0E0i60TlSUD8M",
+    authDomain: "learn-firebase-course.firebaseapp.com",
+    projectId: "learn-firebase-course",
+    storageBucket: "learn-firebase-course.appspot.com",
+    messagingSenderId: "227863067123",
+    appId: "1:227863067123:web:ef51630d3b418984d7ba86",
+    measurementId: "G-4JKX18HYEM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
