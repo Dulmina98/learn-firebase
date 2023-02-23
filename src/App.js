853f5c4d922760ea489b993d220c9b2bb0244b53
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-02-23 18:59:42 +0530

    Create submit movie to firebase functions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import './App.css';
 import {Auth} from "./components/Auth";
 import {db} from "./config/firebase"
 import {useEffect, useState} from "react";
-import {collection, getDocs} from "firebase/firestore"
+import {addDoc, collection, getDocs} from "firebase/firestore"
 
 function App() {
 
     const [movieList, setMovieLIst] = useState([]);
 
     const moviesCollectionRef = collection(db, "movies")
+
+    //new movie states
+    const [newMovieTitle, setNewMovieTitle] = useState("");
+    const [newReleaseDate, setNewReleaseDate] = useState(0);
+    const [newReceivedOscar, setNewReceivedOscar] = useState(false);
+
+    const onsubmitMovie = async () => {
+
+        try {
+            await addDoc(moviesCollectionRef, {
+                title: newMovieTitle,
+                releaseDate: newReleaseDate,
+                recievedOscar: newReceivedOscar
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
 
     useEffect(() => {
         const getMovieList = async () => {
@@ -27,11 +46,23 @@ function App() {
             }
         }
         getMovieList();
-    }, []);
+    }, [onsubmitMovie]);
+
 
     return (
         <div className="App">
             <Auth/>
+
+
+            <div className={"submit-movie"}>
+                <input type="text" placeholder={"Movie title"} onChange={(e) => setNewMovieTitle(e.target.value)}/>
+                <input type="number" placeholder={"Release date"}
+                       onChange={(e) => setNewReleaseDate(Number(e.target.value))}/>
+                <label>Received an oscar</label>
+                <input type="checkbox" checked={newReceivedOscar}
+                       onChange={(e) => setNewReceivedOscar(e.target.checked)}/>
+                <button onClick={onsubmitMovie}>Submit movie</button>
+            </div>
 
             <div>{movieList.map((movie) => (
                 <div>
@@ -41,7 +72,7 @@ function App() {
                 </div>
             ))}</div>
         </div>
-  );
+    );
 }
 
 export default App;
